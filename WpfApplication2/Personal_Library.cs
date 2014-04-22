fe4ed45e19ae30daa62f5258a85f6c60075fb861
K1wi <erick.vdlinde@gmail.com>
2014-04-22 16:13:58 +0200

    Initial Commit of Personal_Library
    The first version of Personal_Library

---
+++ WpfApplication2/Personal_Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Library
+{
+    public class FUNCTIONS
+    {
+        public static void tstFUNC()
+        {
+
+        }
+        public void tstfn()
+        {
+            Console.WriteLine("KKKK");
+        }
+    }
+    
+}
